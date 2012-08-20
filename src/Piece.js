f5ba252bec6636e4bd16303c266c1f96555e4a94
Mark Veltzer <mark.veltzer@gmail.com>
2012-08-20 14:44:46 +0300

    better documentation

--- src/Piece.js
+++ src/Piece.js
@@ -1,16 +1,24 @@
 /**
 	@class represents a position + graphics
-	@constructor
 	@param gr graphics (raphael) for the piece
 	@param pos position for the piece
 	@param pixelPos position for the pieces origin. This is important to be able to move it to other places 
 	pixelPos is not the translation of pos to pixels!!!
+	@param pieceColor the color of the piece ('white','black')
+	@param pieceType the type of the piece ('rook','knight','bishop','queen','king','pawn')
+	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
 */
-function Piece(gr,pos,pixelPos) {
+function Piece(gr,pos,pixelPos,pieceColor,pieceType) {
 	this.gr=gr
 	this.pos=pos
 	this.pixelPos=pixelPos
+	this.pieceColor=pieceColor
+	this.pieceType=pieceType
 }
+/**
+	Debug method that allows you to get a nice printout for this type
+	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+*/
 Piece.prototype.toString=function() {
 	throw 'the toString method still has to be written'
 }
--- src/PieceDesc.js
+++ src/PieceDesc.js
@@ -1,16 +1,20 @@
 /**
 	A single piece description.
 	This includes: rectangle size (assumes piece is 0,0,rect,rect)
-	paths and attributes to draw the path
+	and array of paths and attributes to draw the path
 	@class A single piece description.
-	@constructor
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
 	@param rect rect size
+	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
 */
 function PieceDesc(rect) {
 	this.rect=rect
 	this.paas=[]
 }
+/**
+	Adds a new path section to a piece description
+	@param paa PathAndAttributes object to be added
+	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+*/
 PieceDesc.prototype.add=function(paa) {
 	this.paas.push(paa)
 }
