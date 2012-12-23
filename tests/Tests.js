dc42e7216101cb3d0680ca8198f4cbd3b6cfeb09
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-23 16:24:52 +0200

    better tests

--- tests/Tests.js
+++ tests/Tests.js
@@ -1,6 +1,6 @@
 /*jsl:import PiecePosition.js*/
-test( "hello test", function() {
-	assert.ok( 1 == "1", "Passed!" );
+test( "basic hellow world test", function() {
+	assert.ok( 1 == 1, "Passed!" );
 });
 test( "check that positions cannot be negative", function() {
 	assert.throws(function() {
