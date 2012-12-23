aeb58049b406aa878d5a3eb3079d4daa9ea30e4b
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-23 13:57:27 +0200

    added mako to the mix

---
+++ scripts/mako_wrapper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import sys
+import mako.template
+import mako.lookup
+import os # for os.chmod, os.unlink
+
+import versioncheck
+
+if len(sys.argv)!=3:
+	raise ValueError('command line issue')
+
+input_encoding='utf-8'
+output_encoding='utf-8'
+p_input=sys.argv[1]
+p_output=sys.argv[2]
+
+def get_attr():
+	attr={}
+	attr['ver']="version"
+	return attr
+
+try:
+	os.unlink(p_output)
+except:
+	# handle the error better, only non existant file should be glossed over...
+	pass
+try:
+	mylookup = mako.lookup.TemplateLookup(directories=['.'],input_encoding=input_encoding,output_encoding=output_encoding)
+	template=mako.template.Template(filename=p_input,lookup=mylookup,output_encoding=output_encoding,input_encoding=input_encoding)
+	file=open(p_output,'w')
+	# python 3
+	#file.write((template.render_unicode(attributes={})))
+	# python 2
+	file.write(template.render(**get_attr()))
+	file.close()
+	# python 3
+	#os.chmod(p_output,0o0444)
+	# python 2
+	os.chmod(p_output,0444)
+except Exception,e:
+	print "unlinking output"
+	os.unlink(p_output)
+	raise e
