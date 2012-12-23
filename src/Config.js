23505d8d3b1183c60147abf0b0043057af8adf9f
Mark Veltzer <mark@veltzer.net>
2012-12-23 12:05:17 +0200

    Config documented not to be a singleton

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 /**
-	@class A singleton type safe config class
+	@class Type safe config class
 	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
 	The idea is that the user will not be able to accidently put config
 	options which are not used and will only be able to supply the right
@@ -8,6 +8,8 @@
 	(div id where to create some HTML elements is an example of this).
 	Config will also supply a method by which config options by the user
 	will override anything in the default config.
+	This class SHOULD NOT be a singleton since the user may want to put
+	two boards on the page and have each configured differently.
 */
 function Config() {
 }
