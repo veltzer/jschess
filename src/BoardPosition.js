b324611def6377690f00c7dff283189938d6bbfc
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-24 13:56:55 +0200

    added basic FEN parsing

--- src/BoardPosition.js
+++ src/BoardPosition.js
@@ -42,6 +42,7 @@ BoardPosition.prototype.forEachPiece=function(f) {
 	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
 */
 BoardPosition.startPos=function() {
+	/*
 	var newPos=new BoardPosition();
 	newPos.addPiece('white','rook',0,0);
 	newPos.addPiece('white','knight',1,0);
@@ -76,5 +77,73 @@ BoardPosition.startPos=function() {
 	newPos.addPiece('black','pawn',5,6);
 	newPos.addPiece('black','pawn',6,6);
 	newPos.addPiece('black','pawn',7,6);
+	return newPos;
+	*/
+	return BoardPosition.setupFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+};
+/**
+	Setup a position according to FEN notation.
+	See <a href="http://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation">Forsyth–Edwards Notation</a> for more details.
+	Example of start position is:
+	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+*/
+BoardPosition.setupFEN=function(fen) {
+	var blocks=fen.split(' ');
+	if(blocks.length!=6) {
+		throw 'parse error - number of blocks is not 6';
+	}
+	var ranks=blocks[0].split('/');
+	if(ranks.length!=8) {
+		throw 'parse error - number of ranks is not 8';
+	}
+	var newPos=new BoardPosition();
+	for(var irank=7;irank>=0;irank--) {
+		var rank=ranks[7-rank];
+		for(var iletter=0;iletter<rank.length;iletter++) {
+			var letter=rank[iletter];
+			switch(letter) {
+				case 'r':
+					newPos.addPiece('black','rook',iletter,irank);
+					break;
+				case 'R':
+					newPos.addPiece('white','rook',iletter,irank);
+					break;
+				case 'n':
+					newPos.addPiece('black','knight',iletter,irank);
+					break;
+				case 'N':
+					newPos.addPiece('white','knight',iletter,irank);
+					break;
+				case 'b':
+					newPos.addPiece('black','bishop',iletter,irank);
+					break;
+				case 'B':
+					newPos.addPiece('white','bishop',iletter,irank);
+					break;
+				case 'q':
+					newPos.addPiece('black','queen',iletter,irank);
+					break;
+				case 'Q':
+					newPos.addPiece('white','queen',iletter,irank);
+					break;
+				case 'k':
+					newPos.addPiece('black','king',iletter,irank);
+					break;
+				case 'K':
+					newPos.addPiece('white','king',iletter,irank);
+					break;
+				case 'p':
+					newPos.addPiece('black','pawn',iletter,irank);
+					break;
+				case 'P':
+					newPos.addPiece('white','pawn',iletter,irank);
+					break;
+				default:
+					iletter+=Number(letter)-1;
+					break;
+			}
+		}
+	}
 	return newPos;
 };
