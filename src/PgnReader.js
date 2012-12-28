f8f77efb68a6f041f2be83edcfe741d7ed0cb4d1
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-29 01:50:20 +0200

    more prototype.js movement

--- src/PgnReader.js
+++ src/PgnReader.js
@@ -1,25 +1,29 @@
-/**
-	@class A PGN reader. A class that knows how to read a PGN file and give instructions
-	to a board.
-	@description creates a new instance
-	@constructs
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-function PgnReader() {
-}
-/**
-	@description toString method so that you can get a nice printout of instances of this type
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PgnReader.prototype.toString=function() {
-	throw 'this is not implemented yet';
-};
-/**
-	@description A method to read a pgn file via ajax.
-	@param url url to do the GET from (same server)
-	@param func a function to be called once the game is parsed
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PgnReader.prototype.get=function() {
-	// use jQuery to do HTTP GET
-};
+var PgnReader=Class.create(
+	/** @lends PgnReader# */
+{
+	/**
+		@class A PGN reader. A class that knows how to read a PGN file and give instructions
+		to a board.
+		@description creates a new instance
+		@constructs
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	initialize: function() {
+	},
+	/**
+		@description toString method so that you can get a nice printout of instances of this type
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	toString: function() {
+		throw 'this is not implemented yet';
+	},
+	/**
+		@description A method to read a pgn file via ajax.
+		@param url url to do the GET from (same server)
+		@param func a function to be called once the game is parsed
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	get: function() {
+		// use jQuery to do HTTP GET
+	}
+});
--- src/PieceType.js
+++ src/PieceType.js
@@ -1,23 +1,75 @@
-/**
-	@class represents a piece type (rook,knight,bishop,queen,king,pawn) 
-	@description creates a new instance
-	@param string - the type of the piece
-	@constructs
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-function PieceType(type) {
-	if(!(type in PieceType.types)) {
-		throw 'illegal piecetype '+type;
+var PieceType=Class.create(
+	/** @lends PieceType# */
+{
+	/**
+		@class represents a piece type (rook,knight,bishop,queen,king,pawn) 
+		@description creates a new instance
+		@param string - the type of the piece
+		@constructs
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	initialize: function(type) {
+		if(!(type in PieceType.types)) {
+			throw 'illegal piecetype '+type;
+		}
+		this.type=type;
+	},
+	/**
+		@description toString method that allows you to get a nice printout for this type
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	toString: function() {
+		return this.type;
+	},
+	/**
+		@description Return whether the piece is a rook
+		@returns is this piece a rook
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isRook: function() {
+		return this.type=='rook';
+	},
+	/**
+		@description Return whether the piece is a knight
+		@returns is this piece a knight
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isKnight: function() {
+		return this.type=='knight';
+	},
+	/**
+		@description Return whether the piece is a bishop
+		@returns is this piece a bishop
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isBishop: function() {
+		return this.type=='bishop';
+	},
+	/**
+		@description Return whether the piece is a queen
+		@returns is this piece a queen
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isQueen: function() {
+		return this.type=='queen';
+	},
+	/**
+		@description Return whether the piece is a king
+		@returns is this piece a king
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isKing: function() {
+		return this.type=='king';
+	},
+	/**
+		@description Return whether the piece is a pawn
+		@returns is this piece a pawn
+		@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
+	*/
+	isPawn: function() {
+		return this.type=='pawn';
 	}
-	this.type=type;
-}
-/**
-	@description toString method that allows you to get a nice printout for this type
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.toString=function() {
-	return this.type;
-};
+});
 /**
 	@description Array of piece types
 	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
@@ -29,52 +81,4 @@ PieceType.types={
 	queen:undefined,
 	king:undefined,
 	pawn:undefined
-};
-/**
-	@description Return whether the piece is a rook
-	@returns is this piece a rook
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isRook=function() {
-	return this.type=='rook';
-};
-/**
-	@description Return whether the piece is a knight
-	@returns is this piece a knight
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isKnight=function() {
-	return this.type=='knight';
-};
-/**
-	@description Return whether the piece is a bishop
-	@returns is this piece a bishop
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isBishop=function() {
-	return this.type=='bishop';
-};
-/**
-	@description Return whether the piece is a queen
-	@returns is this piece a queen
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isQueen=function() {
-	return this.type=='queen';
-};
-/**
-	@description Return whether the piece is a king
-	@returns is this piece a king
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isKing=function() {
-	return this.type=='king';
-};
-/**
-	@description Return whether the piece is a pawn
-	@returns is this piece a pawn
-	@author <a href="mailto:mark.veltzer@gmail.com">Mark Veltzer</a>
-*/
-PieceType.prototype.isPawn=function() {
-	return this.type=='pawn';
 };
