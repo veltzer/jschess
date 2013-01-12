01a2c61a024fdb991f768ba6244405157e419103
Mark Veltzer <mark.veltzer@gmail.com>
2013-01-12 11:53:50 +0200

    removed escaping in html files in favour of mako

--- scripts/mako_wrapper.py
+++ scripts/mako_wrapper.py
@@ -42,7 +42,6 @@ def get_attr():
 	attr['copyright_years']=years
 	attr['deps']=deps.deps
 	attr['jsThirdParty']=deps.getJsThirdParty
-	attr['jsThirdPartyEscape']=deps.getJsThirdPartyEscape
 	attr['jsThirdPartyDebug']=deps.getJsThirdPartyDebug
 	attr['jsFiles']=jsFiles
 	return attr
