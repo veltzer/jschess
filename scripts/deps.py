ceb45d15ce52ad2a5ba8b62318842c003fde13c0
Mark Veltzer <mark@veltzer.net>
2013-04-15 18:22:38 +0300

    better versions

--- scripts/deps.py
+++ scripts/deps.py
@@ -8,11 +8,12 @@ module to help handle javascript dependencies for a piece of software
 """
 
 class Dep:
-	def __init__(self,name,version,website,downloadUrl,myFile,myFileDebug,documentation):
+	def __init__(self,name,version,website,downloadUrl,downloadUrlDebug,myFile,myFileDebug,documentation):
 		self.name=name
 		self.version=version
 		self.website=website
 		self.downloadUrl=downloadUrl
+		self.downloadUrlDebug=downloadUrlDebug
 		self.myFile=myFile
 		self.myFileDebug=myFileDebug
 		self.documentation=documentation
@@ -22,6 +23,7 @@ deps.append(Dep(
 	'prototype',
 	'1.7.1',
 	'http://prototypejs.org',
+	'not available from the web',
 	'https://ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js',
 	'thirdparty/prototype-1.7.1.min.js',
 	'thirdparty/prototype-1.7.1.js',
@@ -32,6 +34,7 @@ deps.append(Dep(
 	'2.1.0',
 	'http://raphaeljs.com',
 	'http://github.com/DmitryBaranovskiy/raphael/raw/master/raphael-min.js',
+	'http://github.com/DmitryBaranovskiy/raphael/raw/master/raphael.js',
 	'thirdparty/raphael-2.1.0.min.js',
 	'thirdparty/raphael-2.1.0.js',
 	'http://raphaeljs.com/reference.html',
@@ -40,7 +43,8 @@ deps.append(Dep(
 	'chess.js',
 	'v0.1-47-gb7c9788',
 	'https://github.com/jhlywa/chess.js',
-	'https://github.com/jhlywa/chess.js/blob/master/chess.min.js',
+	'https://github.com/jhlywa/chess.js/raw/master/chess.min.js',
+	'https://github.com/jhlywa/chess.js/raw/master/chess.js',
 	'thirdparty/chess.min.js',
 	'thirdparty/chess.js',
 	'https://github.com/jhlywa/chess.js/blob/master/README.md',
