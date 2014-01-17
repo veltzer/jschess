577eb870df47d180319a5114716d38ac181e115c
Mark Veltzer <mark.veltzer@gmail.com>
2014-01-17 22:51:10 +0200

    removed unneeded function

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -3,21 +3,10 @@
 # run any command line and do not emit it's standard error or output unless there is an error
 
 import sys # for argv
-import subprocess # for Popen
+import subprocess # for check_output
 
-# this function is here because python2.6 does not have subprocess.check_output
-def system_check_output(args):
-	pr=subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	(output,errout)=pr.communicate()
-	status=pr.returncode
-	if status:
-		print output
-		print errout
-		sys.exit(status);
-		#raise ValueError('error in executing',args)
-
-if len(sys.argv)<1:
+if len(sys.argv)<2:
 	raise ValueError('command line issue')
 
 # run the command
-system_check_output(sys.argv[1:])
+subprocess.check_output(sys.argv[1:])
