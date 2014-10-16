ef3bd20bfbb2f29e4414bca3a1a9142098517f1a
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-16 10:54:28 +0300

    script change

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # this script will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-import subprocess
+import subprocess # for check_call
+
 packs=[
 	'yui-compressor',
 	'jsdoc-toolkit',
