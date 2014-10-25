29c4a0984974f4549b1928d8f7b9f85ee0cbac9a
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-25 12:29:18 +0300

    even more nodejs stuff

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -8,6 +8,10 @@ ubuntu to compile and work with this package.
 import subprocess # for check_call
 
 packs=[
+	# nodejs and npm for installing javascript packages
+	'nodejs',
+	'npm',
+
 	'yui-compressor',
 	'jsdoc-toolkit',
 	'closure-linter',
