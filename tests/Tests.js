78003c63d51d69bf67625d38314e9775c66adfa1
Mark Veltzer <mark.veltzer@gmail.com>
2016-06-27 04:01:48 +0300

    fixed up the tests

--- tests/Tests.js
+++ tests/Tests.js
@@ -1,8 +1,8 @@
 /*jsl:import PiecePosition.js*/
-test( "basic hellow world test", function() {
+QUnit.test( "basic hellow world test", function() {
 	assert.ok( 1 == 1, "Passed!" );
 });
-test( "check that positions cannot be negative", function() {
+QUnit.test( "check that positions cannot be negative", function() {
 	assert.throws(function() {
 		new PiecePosition(-5,-6)
 	});
