879ff142bbb9ad46933bcac0441f38506f39b308
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-08 14:57:06 +0300

    reorg

--- scripts/tools.py
+++ scripts/tools.py
@@ -18,6 +18,33 @@ import urllib.request # for urlretrieve
 sys.path.append(os.getcwd())
 import templardefs.jschess
 
+#############
+# functions #
+#############
+
+def install_closure():
+	print('installing tool [{0}]'.format('closure'))
+	if not os.path.isdir(tools):
+		os.mkdir(tools)
+	#jar_name='compiler.jar'
+	jar_name='closure-compiler-v20160713'
+	os.system('wget -qO- https://dl.google.com/closure-compiler/compiler-latest.zip | (cd tools; bsdtar -xf- {jar_name}.jar)'.format(jar_name=jar_name))
+	os.chmod('tools/{jar_name}.jar'.format(jar_name=jar_name), 0o0775)
+
+def install_jsmin():
+	print('installing tool [{0}]'.format('jsmin'))
+	if not os.path.isdir(tools):
+		os.mkdir(tools)
+	os.system('wget -qO- https://raw.githubusercontent.com/douglascrockford/JSMin/master/jsmin.c | (cd tools; gcc -x c -O2 - -o jsmin)')
+
+def install_jsl():
+	print('installing tool [{0}]'.format('jsl'))
+	if not os.path.isdir(tools):
+		os.mkdir(tools)
+	#os.system('wget -qO- http://www.javascriptlint.com/download/jsl-0.3.0-src.tar.gz | (cd tools; tar zxf -)')
+	#os.system('cd tools; python setup.py build')
+	os.system('cd tools; svn -q co https://javascriptlint.svn.sourceforge.net/svnroot/javascriptlint/trunk jsl')
+	os.system('cd tools/jsl; python setup.py build > /dev/null')
 ##############
 # parameters #
 ##############
@@ -44,6 +71,12 @@ packs=[
 ########
 # code #
 ########
+if os.path.isdir(tools):
+	shutil.rmtree(tools)
+install_closure()
+install_jsmin()
+install_jsl()
+
 for pack in packs:
 	print('getting ubuntu package for [{0}]'.format(pack))
 	subprocess.check_call([
@@ -82,7 +115,7 @@ for dep in templardefs.jschess.deps:
 		if debug:
 			print('doing closure')
 		subprocess.check_call([
-			'/home/mark/install/closure/closure.jar',
+			'tools/closure-compiler-v20160713.jar',
 			dep.myFileDebug,
 			'--js_output_file',
 			dep.myFile,
@@ -91,7 +124,7 @@ for dep in templardefs.jschess.deps:
 		if debug:
 			print('doing jsmin')
 		subprocess.check_call([
-			'/home/mark/install/jsmin/jsmin',
+			'tools/jsmin',
 			],
 			stdout=open(dep.myFile, 'w'),
 			stdin=open(dep.myFileDebug),
@@ -106,22 +139,3 @@ for f in os.listdir(tp):
 		if debug:
 			print('chmodding [{0}]'.format(full))
 		os.chmod(full, 0o0444)
-
-if os.path.isdir(tools):
-	shutil.rmtree(tools)
-os.mkdir(tools)
-
-print('installing tool [{0}]'.format('closure'))
-#jar_name='compiler.jar'
-jar_name='closure-compiler-v20160713'
-os.system('wget -qO- https://dl.google.com/closure-compiler/compiler-latest.zip | (cd tools; bsdtar -xf- {jar_name}.jar)'.format(jar_name=jar_name))
-os.chmod('tools/{jar_name}.jar'.format(jar_name=jar_name), 0o0775)
-
-print('installing tool [{0}]'.format('jsmin'))
-os.system('wget -qO- https://raw.githubusercontent.com/douglascrockford/JSMin/master/jsmin.c | (cd tools; gcc -x c -O2 - -o jsmin)')
-
-print('installing tool [{0}]'.format('jsl'))
-#os.system('wget -qO- http://www.javascriptlint.com/download/jsl-0.3.0-src.tar.gz | (cd tools; tar zxf -)')
-#os.system('cd tools; python setup.py build')
-os.system('cd tools; svn -q co https://javascriptlint.svn.sourceforge.net/svnroot/javascriptlint/trunk jsl')
-os.system('cd tools/jsl; python setup.py build > /dev/null')
