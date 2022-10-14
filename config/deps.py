11bd8d5e071a462d4c71ad48adc7f5b288ce2653
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-14 19:00:59 +0300

    deps change

--- config/deps.py
+++ config/deps.py
@@ -1,26 +1,26 @@
 packages=[
     # for installing javascript packages
-    'nodejs',
+    "nodejs",
     # for installing javascript packages
-    'npm',
+    "npm",
     # for yui-compressor(1)
-    'yui-compressor',
+    "yui-compressor",
     # for jsdoc(1)
-    'jsdoc-toolkit',
+    "jsdoc-toolkit",
     # for gjslint(1)
-    #'closure-linter',
+    #"closure-linter",
     # for tidy(1)
-    'tidy',
+    "tidy",
     # for zip(1)
-    'zip',
+    "zip",
     # for sloccount(1)
-    'sloccount',
+    "sloccount",
     # for bsdtar(1)
-    'libarchive-tools',
+    "libarchive-tools",
 ]
 tools=[
-    'jsl',
-    'jsmin',
-    'closure',
-    'css-validator',
+    "jsl",
+    "jsmin",
+    "closure",
+    "css-validator",
 ]
